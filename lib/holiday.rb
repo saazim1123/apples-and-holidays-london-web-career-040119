46dbf9a8cc3c20ec75ebdbc50a6da8e0e5cde134
Adnan Azim <saazim1123@gmail.com>
2019-03-14 17:48:35 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -23,18 +23,29 @@ def second_supply_for_fourth_of_july(holiday_hash)
   
 end
 
-def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
- 
-  holiday_hash[:winter].each do |holiday, supplies|
-    supplies << supply
-  end
-  
-  
-end
 
+def all_supplies_in_holidays(holiday_hash)
+  # iterate through holiday_hash and print items such that your readout resembles:
+  # Winter:
+  #   Christmas: Lights, Wreath
+  #   New Years: Party Hats
+  # Summer:
+  #   Fourth Of July: Fireworks, BBQ
+  # etc.
+    holiday_hash.each do |season, holidays|
+    puts "#{season.to_s.capitalize}:"
+    
+    holidays.each do |holiday, supplies|
+    puts "  #{holiday.to_s.split("_").collect{|s| s.capitalize}.join(" ")}: #{supplies.join(", ")}"
+    
+   
+ 
+    
+    end
+  end
+
+      
+end
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
@@ -56,30 +67,41 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].values.flatten
 end
 
-def all_supplies_in_holidays(holiday_hash)
-  # iterate through holiday_hash and print items such that your readout resembles:
-  # Winter:
-  #   Christmas: Lights, Wreath
-  #   New Years: Party Hats
-  # Summer:
-  #   Fourth Of July: Fireworks, BBQ
-  # etc.
-  
-  holiday_hash.each do |season, holidays|
-  
-  
+
+
+
+def add_supply_to_winter_holidays(holiday_hash, supply)
+  # holiday_hash is identical to the one above
+  # add the second argument, which is a supply, to BOTH the
+  # Christmas AND the New Year's arrays
+ 
+  holiday_hash[:winter].each do |holiday, supplies|
+    supplies << supply
+  end
   
   
 end
+
+
+
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  
+  holiday_hash.collect { |season, holidays|
+    holidays.collect do |holiday, supplies|
+      if supplies.include?("BBQ")
+        holiday
+      end
+    end
+  }.flatten.compact
+  
+ 
+
+
 
 end
-
-
-
 
 
 
